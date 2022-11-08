aeff94c13ac42c3b93884ab74a25a3273ea3466a
Baito838 <bagas.aji838@gmail.com>
2022-11-08 18:30:44 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PASWA 2022</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <style>
+        .container{
+            margin-top: 30px;
+            height: 200px;
+            position: sticky;
+        }
+        .nav {
+            display: flex;
+            justify-content: space-between;
+        }
+        .tombol {
+            display: flex;
+            justify-content: center;
+            gap:20px;
+        }
+    </style>
+</head>
+
+<body class="bg-gradient-dark">
+    <div class="container">
+        <div class="nav">
+            <h1 class="text-center text-white">Paswa <sub>2022</sub></h1>
+            <div class="tombol">
+            <div><a class="btn btn-success" href="login.php">Login</a></div>
+            <div><a class="btn btn-warning" href="register.php">Register</a></div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
